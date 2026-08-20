6272dfc50b1d28a22def8cd58445ad675dfd8c01
gaku-base <mail.skgc@gmail.com>
2026-08-21 03:33:36 +0900

    test: scaffold presentation mode regression suite

---
+++ presentation-mode.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const test = require('node:test');
+const assert = require('node:assert/strict');
+
+test('presentation mode scaffold is tracked', () => {
+  assert.equal(true, true);
+});
