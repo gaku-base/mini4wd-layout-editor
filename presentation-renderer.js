3b8fc0b369c1a63fe54aac4969bd4fbee2471a7e
gaku-base <mail.skgc@gmail.com>
2026-08-21 03:34:32 +0900

    feat: scaffold presentation renderer

---
+++ presentation-renderer.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Presentation-only 2D renderer. Implemented in P2.
+(function registerPresentationRenderer(global) {
+  global.M4WD_PRESENTATION_RENDERER = Object.freeze({ version: 1 });
+})(window);
