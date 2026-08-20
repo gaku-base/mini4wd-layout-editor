2bb54d9d830a87622d4cfc7e33fe3e5d4fde80cf
gaku-base <mail.skgc@gmail.com>
2026-08-20 15:54:59 +0900

    test: update cache-safe UI asset version

--- cache-safe-ui-assets.test.js
+++ cache-safe-ui-assets.test.js
@@ -2,7 +2,7 @@ const test = require('node:test');
 const assert = require('node:assert/strict');
 const fs = require('node:fs');
 
-const VERSION = 'v1.1-rc4-20260820-overlap2';
+const VERSION = 'v1.1-rc4-20260820-toolbar-trash1';
 
 test('sub-edit overlap suppression is static and critical UI assets are cache-busted', () => {
   const index = fs.readFileSync('index.html', 'utf8');
