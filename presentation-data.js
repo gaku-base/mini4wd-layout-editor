c7e5b8cd1930019d1e11d6a0f7ab8b565ece03e3
gaku-base <mail.skgc@gmail.com>
2026-08-21 03:34:11 +0900

    feat: scaffold presentation data helpers

---
+++ presentation-data.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Pure presentation data helpers live here so they can be unit-tested without touching editor state.
+(function registerPresentationData(global) {
+  global.M4WD_PRESENTATION_DATA = Object.freeze({ version: 1 });
+})(window);
