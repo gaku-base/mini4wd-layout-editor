5bb5fdb0251631c25d0f7956508c72b9b940768a
gaku-base <mail.skgc@gmail.com>
2026-08-21 03:34:53 +0900

    feat: scaffold presentation export helpers

---
+++ presentation-export.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// PNG and print export helpers. Implemented in P4/P5.
+(function registerPresentationExport(global) {
+  global.M4WD_PRESENTATION_EXPORT = Object.freeze({ version: 1 });
+})(window);
